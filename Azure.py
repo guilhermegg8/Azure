60c9c16d3bfe1eda061d655d85ffbd3bb80bfde2
guilhermegg8 <guiguigoncalvess@gmail.com>
2024-10-14 10:21:12 -0300

    Refactor Azure.py and update AzureOpenAI client initialization

--- Azure.py
+++ Azure.py
@@ -5,14 +5,12 @@ endpoint = os.getenv("ENDPOINT_URL", "https://guilh-m28xpur1-australiaeast.opena
 deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4-32k")
 subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "AZURE_OPENAI_API_KEY")
 
-# Inicializar o cliente OpenAI do Azure com autenticação baseada em chave
 client = AzureOpenAI(
     azure_endpoint=endpoint,
     api_key=subscription_key,
     api_version="2024-05-01-preview",
 )
 
-# Gerar a conclusão
 completion = client.chat.completions.create(
     model=deployment,
     messages=[{"role": "user", "content": "Principais serviços do Azure para desenvolvedores de softwares"}],
